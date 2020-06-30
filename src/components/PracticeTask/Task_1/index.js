2ebb6aff9071509496a348b2b0dddcd8925f42b1
W-Shakespeare <kiiiikkiiiik2@gmail.com>
2020-07-01 01:29:30 +0300

    Added style.css for App

--- src/components/PracticeTask/Task_1/index.js
+++ src/components/PracticeTask/Task_1/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import NameGenerator from "./NameGenerator";
-
+import "./style.css";
 export default class PracticeTask extends Component {
   render() {
     return (
