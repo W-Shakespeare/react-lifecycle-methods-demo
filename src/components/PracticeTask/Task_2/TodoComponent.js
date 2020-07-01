14d9d95a46dbf4e9e529475d1fbb785813a34d6e
W-Shakespeare <kiiiikkiiiik2@gmail.com>
2020-07-02 01:03:50 +0300

    Created input type text in TodoComponent and added event handler this input

--- src/components/PracticeTask/Task_2/TodoComponent.js
+++ src/components/PracticeTask/Task_2/TodoComponent.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-function TodoComponent({ todo, id }) {
-  return <li key={id}>{todo}</li>;
+function TodoComponent({ todo, id, inputValue, handleChange }) {
+  return (
+    <div>
+      <input onChange={handleChange} type="text" value={inputValue}></input>
+      <li key={id}>{todo}</li>;
+    </div>
+  );
 }
 
 export default TodoComponent;
--- src/components/PracticeTask/Task_2/TodoContainer.js
+++ src/components/PracticeTask/Task_2/TodoContainer.js
@@ -5,14 +5,29 @@ class TodoContainer extends Component {
     super(props);
     this.state = {
       arrTodo: [{ id: Date.now(), todo: "First Todo" }],
+      inputValue: "",
     };
   }
+
+  handleChange = (e) => {
+    this.setState({
+      inputValue: e.target.value,
+    });
+  };
+
   render() {
     return (
       <div>
         <ul>
           {this.state.arrTodo.map((objTodo) => {
-            return <TodoComponent todo={objTodo.todo} id={objTodo.id} />;
+            return (
+              <TodoComponent
+                todo={objTodo.todo}
+                id={objTodo.id}
+                inputValue={this.state.inputValue}
+                handleChange={this.handleChange}
+              />
+            );
           })}
         </ul>
       </div>
