45bf503edd1c272c205377f62d583ade3b062aa3
W-Shakespeare <kiiiikkiiiik2@gmail.com>
2020-07-04 22:29:17 +0300

    Start Task_8. Created index.js. Added BookmarkComponent

---
+++ src/components/PracticeTask/Task_8/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./BookMarkContainer";
+export default class PracticeTask extends Component {
+  render() {
+    return (
+      <div>
+        <BookmarkComponent />
+      </div>
+    );
+  }
+}
