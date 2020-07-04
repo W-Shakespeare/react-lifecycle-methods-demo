eb82380a9c56312e4ab385d443df5ef77c3a73b2
W-Shakespeare <kiiiikkiiiik2@gmail.com>
2020-07-04 21:52:19 +0300

    Added ShowPictureComponent. Created onImgChange func.Transfer props

--- src/components/PracticeTask/Task_7/ShowPictureContainer.js
+++ src/components/PracticeTask/Task_7/ShowPictureContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import ShowPictureComponent from "./ShowPictureComponent";
 export default class ShowPictureContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
       user: null,
       loading: true,
+      currentImg: 28,
     };
   }
   componentDidMount() {
@@ -24,16 +26,25 @@ export default class ShowPictureContainer extends Component {
         });
       });
   };
+
+  onImgChange = (position) => {
+    position = position < 0 ? 29 : position > 29 ? 0 : position;
+    this.setState({
+      currentImg: position,
+    });
+  };
   render() {
     return (
       <div className="wrapper">
         {this.state.loading ? (
           <p>...Loading</p>
         ) : (
-          <div>
-            <img src={this.state.user[0].download_url}></img>
-            <p>{this.state.user[0].author}</p>
-          </div>
+          <ShowPictureComponent
+            onImgChange={this.onImgChange}
+            position={this.state.currentImg}
+            author={this.state.user[this.state.currentImg].author}
+            url={this.state.user[this.state.currentImg].download_url}
+          />
         )}
       </div>
     );
